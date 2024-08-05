288b73f0c6dddd75da379752e18a5dd3bd52ff42
joshua hammer <jph33@outlook.com>
2024-08-05 15:40:55 -0400

    tmux is back to tokyonight

--- modules/homemodules/tmux.nix
+++ modules/homemodules/tmux.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     shortcut = "Space";
@@ -10,15 +6,12 @@
     clock24 = true;
     keyMode = "vi";
     baseIndex = 1;
-    plugins = with pkgs.tmuxPlugins; [
-      sensible
-      tilish
-      tmux-fzf
-    ];
+    plugins = with pkgs.tmuxPlugins; [sensible tilish tmux-fzf];
     extraConfig = ''
       set -g mouse on
       set-option -ga terminal-overrides ",xterm-256color:Tc"
 
+      set -g status-right "#[fg=colour133,nobold,nounderscore,noitalics]#[fg=colour0,bg=colour133] #h "
       bind v split-window -h -c '#{pane_current_path}'
       bind s split-window -v -c '#{pane_current_path}'
       bind c new-window -c '#{pane_current_path}'
@@ -27,18 +20,19 @@
       set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
       set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
 
-      set -g mode-style "fg=#${config.stylix.base16Scheme.base04},bg=#${config.stylix.base16Scheme.base00}"
+      # tokyonight-night
+      set -g mode-style "fg=#7aa2f7,bg=#3b4261"
 
-      set -g message-style "fg=#${config.stylix.base16Scheme.base04},bg=#${config.stylix.base16Scheme.base00}"
-      set -g message-command-style "fg=#${config.stylix.base16Scheme.base04},bg=#${config.stylix.base16Scheme.base00}"
+      set -g message-style "fg=#7aa2f7,bg=#3b4261"
+      set -g message-command-style "fg=#7aa2f7,bg=#3b4261"
 
-      set -g pane-border-style "fg=#${config.stylix.base16Scheme.base00}"
-      set -g pane-active-border-style "fg=#${config.stylix.base16Scheme.base0C}"
+      set -g pane-border-style "fg=#3b4261"
+      set -g pane-active-border-style "fg=#7aa2f7"
 
       set -g status "on"
       set -g status-justify "left"
 
-      set -g status-style "fg=#${config.stylix.base16Scheme.base07},bg=#${config.stylix.base16Scheme.base00}"
+      set -g status-style "fg=#7aa2f7,bg=#16161e"
 
       set -g status-left-length "100"
       set -g status-right-length "100"
@@ -46,18 +40,18 @@
       set -g status-left-style NONE
       set -g status-right-style NONE
 
-      set -g status-left "#[fg=#${config.stylix.base16Scheme.base00},bg=#${config.stylix.base16Scheme.base0D},bold] #S #[fg=#${config.stylix.base16Scheme.base0D},bg=#${config.stylix.base16Scheme.base00},nobold,nounderscore,noitalics]"
-      set -g status-right "#[fg=#${config.stylix.base16Scheme.base0D},nobold,nounderscore,noitalics,bg=#${config.stylix.base16Scheme.base00}]#[fg=#${config.stylix.base16Scheme.base00},bg=#${config.stylix.base16Scheme.base0D}] #h "
+      set -g status-left "#[fg=#15161e,bg=#7aa2f7,bold] #S #[fg=#7aa2f7,bg=#16161e,nobold,nounderscore,noitalics]"
+      set -g status-right "#[fg=#7aa2f7,bg=#16161e,nobold,nounderscore,noitalics]#[fg=#15161e,bg=#7aa2f7,bold] #h "
 
+      setw -g window-status-activity-style "underscore,fg=#a9b1d6,bg=#16161e"
       setw -g window-status-separator ""
-      setw -g window-status-activity-style "underscore,fg=#${config.stylix.base16Scheme.base09},bg=#${config.stylix.base16Scheme.base05}"
-      setw -g window-status-style "NONE,fg=#${config.stylix.base16Scheme.base09},bg=#${config.stylix.base16Scheme.base01}"
-      setw -g window-status-format "#[fg=#${config.stylix.base16Scheme.base00},bg=#${config.stylix.base16Scheme.base01},nobold,nounderscore,noitalics]#[fg=#${config.stylix.base16Scheme.base0D},bg=#${config.stylix.base16Scheme.base01}] #I  #W #F #[fg=#${config.stylix.base16Scheme.base01},bg=#${config.stylix.base16Scheme.base00},nobold,nounderscore,noitalics]"
-      setw -g window-status-current-format "#[fg=#${config.stylix.base16Scheme.base00},bg=#${config.stylix.base16Scheme.base02},nobold,nounderscore,noitalics]#[fg=#${config.stylix.base16Scheme.base06},bg=#${config.stylix.base16Scheme.base02},bold] #I  #W #F #[fg=#${config.stylix.base16Scheme.base02},bg=#${config.stylix.base16Scheme.base00},nobold,nounderscore,noitalics]"
+      setw -g window-status-style "NONE,fg=#a9b1d6,bg=#16161e"
+      setw -g window-status-format "#[fg=#16161e,bg=#16161e,nobold,nounderscore,noitalics]#[default] #I / #W #F #[fg=#16161e,bg=#16161e,nobold,nounderscore,noitalics]"
+      setw -g window-status-current-format "#[fg=#16161e,bg=#3b4261,nobold,nounderscore,noitalics]#[fg=#7aa2f7,bg=#3b4261,bold] #I / #W #F #[fg=#3b4261,bg=#16161e,nobold,nounderscore,noitalics]"
 
       # tmux-plugins/tmux-prefix-highlight support
-      set -g @prefix_highlight_output_prefix "#[fg=#${config.stylix.base16Scheme.base04}]#[bg=#${config.stylix.base16Scheme.base01}]#[fg=#${config.stylix.base16Scheme.base01}]#[bg=#e0af68]"
-      set -g @prefix_highlight_output_suffix ""
+      set -g @prefix_highlight_output_prefix "#[fg=#e0af68]#[bg=#16161e]#[fg=#16161e]#[bg=#e0af68]"
+      set -g @prefix_highlight_output_suffix ""
     '';
   };
 }
