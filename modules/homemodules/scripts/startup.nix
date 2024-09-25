16144f27c86c6f58d63c492ef1030b13583400ac
joshua hammer <jph33@outlook.com>
2024-09-24 21:10:57 -0400

    sway is here yooooooooooooooooo

--- modules/homemodules/scripts/startup.nix
+++ modules/homemodules/scripts/startup.nix
@@ -2,31 +2,18 @@
   pkgs,
   wallpaper ? "/home/joshammer/OneDrive/Documents/Stuff/pics/cybertruckLego.jpg",
   ...
-}: let
-  rg = "${pkgs.ripgrep}/bin/rg";
-in
-  pkgs.writeShellScriptBin "startup" ''
+}:
+pkgs.writeShellScriptBin "startup" ''
 
-    if [[ $(ps -e | ${rg} "X" | ${rg} -v "wayland") ]]; then # Xorg
+  killall .waybar-wrapped
+  waybar &
+  swww-daemon &
+  swww img ${wallpaper}
+  
+  wl-paste --type text --watch cliphist store &
+  wl-paste --type image --watch cliphist store &
+  wl-clip-persist --clipboard regular &
 
-      feh --bg-fill ${wallpaper}
-      start-polybar
-      if [[ $(hostname) == "magnus" ]]; then
-        xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --rate 165.00 --pos 2922x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
-      fi
-
-    else # hypr
-
-      killall .waybar-wrapped
-      waybar &
-      swww-daemon &
-      swww img ${wallpaper}
-
-      wl-paste --type text --watch cliphist store &
-      wl-paste --type image --watch cliphist store &
-      wl-clip-persist --clipboard regular & 
-    fi
-
-    onedrive --monitor &
-    nm-applet &
-  ''
+  onedrive --monitor &
+  nm-applet &
+''
--- modules/hypr/sway.nix
+++ modules/hypr/sway.nix
@@ -1,7 +1,6 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }: {
   options.sway.enable = lib.mkEnableOption "enables sway WMs";
@@ -12,27 +11,88 @@
       wrapperFeatures.gtk = true;
     };
 
-    home-manager.users.joshammer.wayland.windowManager.sway = let
-      s = config.home-manager.users.joshammer.wayland.windowManager.sway;
-    in {
+    home-manager.users.joshammer.wayland.windowManager.sway = {
       enable = true;
-      config = {
+      config = let
+        mod = "Mod4";
+      in {
         input."*" = {
-          xkb_layout = "us, us";
-          xkb_variant = "dvorak, ";
+          xkb_layout = "us";
+          xkb_variant = "dvorak";
           xkb_options = "caps:escape";
-          repeat_delay = 225;
-          repeat_rate = 20;
+          repeat_delay = "225";
+          repeat_rate = "50";
         };
-        modifier = "mod4";
-        keybinds = let
-          m = s.config.modifier;
-        in
-          lib.mkOptionDefault {
-            "${m}+Return" = "exec foot";
-            "${m}+d" = "exec vesktop";
-            "${m}+Shift+q" = "kill";
-          };
+        modifier = mod;
+        keybindings = {
+          "${mod}+return" = "exec foot";
+          "${mod}+shift+return" = "exec kitty";
+
+          "${mod}+b" = "exec brave";
+          "${mod}+shift+b" = "exec firefox";
+
+          "${mod}+d" = "exec vesktop";
+          "${mod}+shift+d" = "exec brave 'https://teams.microsoft.com/v2/'";
+
+          "${mod}+x" = "exec wl-copy 'https://xkcd.com/1475/'";
+          "${mod}+shift+x" = ''exec wl-copy '"Do you feel blame? Are you mad? Do you feel like woosh kabob rob vanish, efranish bw-bwooch pajooj, bea-ramich agij gij gij gij googood, do blegehthethamis sergeant British frazzlebaga?"'';
+
+          "${mod}+n" = "exec dunstctl close-all";
+          "${mod}+shift+n" = "exec dunstctl set-paused true";
+          "${mod}+control+n" = "exec dunstctl set-paused false";
+
+          "${mod}+s" = "exec rofi -show drun";
+          "${mod}+c" = "exec rofi -show calc -modi calc -no-show-match -no-sort -qalc-binary qalc | wl-copy";
+          "${mod}+shift+e" = "exec rofi -modi emoji -show emoji";
+          "${mod}+v" = "exec cliphist list | rofi -dmenu | cliphist decode | wl-copy";
+
+          "${mod}+shift+q" = "kill";
+          "${mod}+control+shift+semicolon" = "exit";
+          "${mod}+shift+z" = "exec poweroff";
+          "${mod}+control+z" = "exec reboot";
+
+          "print" = "exec hyprshot -m output --clipboard-only";
+          "${mod}+shift+print" = "exec, hyprshot -m window --clipboard-only";
+          "${mod}+shift+s" = "exec hyprshot -m region --clipboard-only";
+          "${mod}+control+s" = "exec wl-paste | swappy -f -";
+
+          "${mod}+space" = "floating toggle";
+          "${mod}+shift+space" = "floating toggle";
+
+          "${mod}+a" = "focus parent";
+          "${mod}+shift+a" = "focus child";
+
+          "${mod}+escape" = "exec swaylock -c 000000";
+
+          "${mod}+h" = "focus left";
+          "${mod}+l" = "focus right";
+          "${mod}+j" = "focus down";
+          "${mod}+k" = "focus up";
+
+          "${mod}+shift+h" = "move left";
+          "${mod}+shift+l" = "move right";
+          "${mod}+shift+j" = "move down";
+          "${mod}+shift+k" = "move up";
+
+          "${mod}+t" = "split v";
+          "${mod}+shift+t" = "split h";
+
+          "${mod}+o" = "move workspace to output next";
+          "${mod}+f" = "fullscreen toggle";
+
+          "${mod}+apostrophe" = "workspace number 1";
+          "${mod}+comma" = "workspace number 2";
+          "${mod}+period" = "workspace number 3";
+          "${mod}+p" = "workspace number 4";
+          "${mod}+y" = "workspace number 5";
+
+          "${mod}+shift+apostrophe" = "move container to workspace number 1";
+          "${mod}+shift+comma" = "move container to workspace number 2";
+          "${mod}+shift+period" = "move container to workspace number 3";
+          "${mod}+shift+p" = "move container to workspace number 4";
+          "${mod}+shift+y" = "move container to workspace number 5";
+        };
+        bindkeysToCode = true;
       };
     };
   };
