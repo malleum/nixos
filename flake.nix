dba9faab00a6c0c4eb7c7237d6ab9eb6119bdf1c
joshua hammer <jph33@outlook.com>
2025-01-09 16:39:26 -0500

    flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
     stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "unstable";
-    flake-utils.url = "github:numtide/flake-utils";
 
     alejandra.url = "github:kamadorueda/alejandra/3.0.0";
     base16.url = "github:SenchoPens/base16.nix/665b3c6748534eb766c777298721cece9453fdae";
@@ -14,7 +13,6 @@
     fix-python.url = "github:GuillaumeDesforges/fix-python";
     nix-alien.url = "github:thiagokokada/nix-alien";
     nixvim.url = "github:malleum/nixvim";
-    nxvim.url = "github:nix-community/nixvim";
     rip2.url = "github:MilesCranmer/rip2";
     stylix.url = "github:danth/stylix";
     stylix.inputs.base16.follows = "base16";
@@ -22,10 +20,12 @@
   outputs = inputs: let
     system = "x86_64-linux";
     inherit (inputs.unstable) lib;
+
     ns = host: (lib.nixosSystem {
       specialArgs = {inherit inputs system;};
       modules = [(./hosts + "/${host}")];
     });
+
     pkgs = import inputs.unstable {inherit system;};
     ss = name: {
       type = "app";
