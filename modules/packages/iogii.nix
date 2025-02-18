f2d33e27acc09ebe43ed32b76e9c692477e2d024
joshua hammer <jph33@outlook.com>
2025-02-18 17:13:07 -0500

    iogii hash

--- modules/packages/iogii.nix
+++ modules/packages/iogii.nix
@@ -1,15 +1,12 @@
-{
-  stdenv,
-  ruby,
-}: let
+{pkgs, ...}: let
 in
-  stdenv.mkDerivation {
+  pkgs.stdenv.mkDerivation {
     name = "iogii";
     src = builtins.fetchurl {
       url = "https://golfscript.com/iogii/iogii-0.2-alpha";
       sha256 = "sha256-c1yEN23fG5SePVS4wXoiiUnyBS4K4La3GBYeopFJ97Q=";
     };
-    buildInputs = [ruby];
+    buildInputs = [pkgs.ruby];
     unpackPhase = ":";
     installPhase = ''
       mkdir -p $out/{bin,share/iogii}
@@ -17,7 +14,7 @@ in
       bin=$out/bin/iogii
       cat > $bin <<EOF
           #!/bin/sh -e
-          exec ${ruby}/bin/ruby $out/share/iogii/iogii "\$@"
+          exec ${pkgs.ruby}/bin/ruby $out/share/iogii/iogii "\$@"
       EOF
       chmod +x $bin
     '';
