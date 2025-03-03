93d9716f4f5a419b238873b0710ed3425fbdc35e
joshua hammer <jph33@outlook.com>
2025-03-03 17:39:50 -0500

    so close?

--- flake.nix
+++ flake.nix
@@ -10,27 +10,36 @@
     nix-alien.url = "github:thiagokokada/nix-alien";
     nixvim.url = "github:malleum/nixvim";
     stylix.url = "github:danth/stylix";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = inputs: let
-    system = "x86_64-linux";
-    inherit (inputs.unstable) lib;
+  outputs = inputs:
+    inputs.flake-utils.lib.eachSystem inputs.flake-utils.lib.allSystems (
+      system: let
+        inherit (inputs.unstable) lib;
 
-    ns = host: (lib.nixosSystem {
-      specialArgs = {inherit inputs system;};
-      modules = [(./hosts + "/${host}")];
-    });
+        ns = host: (lib.nixosSystem {
+          specialArgs = {inherit inputs system;};
+          modules = [(./hosts + "/${host}")];
+        });
 
-    pkgs = import inputs.unstable {inherit system;};
-    ss = name: {
-      type = "app";
-      program = "${pkgs.callPackage ./modules/homemodules/scripts/${name}.nix {inherit pkgs;}}/bin/${name}";
-    };
-  in {
-    apps.${system} = lib.attrsets.genAttrs (map (a: builtins.substring 0 (builtins.stringLength (builtins.baseNameOf a) - 4) (builtins.baseNameOf a)) (lib.filesystem.listFilesRecursive ./modules/homemodules/scripts)) ss;
-    devShells.${system} = {
-      default = import ./shell.nix {inherit pkgs;};
-      scripts = import ./modules/homemodules/shell.nix {inherit pkgs;};
-    };
-    nixosConfigurations = lib.attrsets.genAttrs ["malleum" "magnus" "minimus"] ns;
-  };
+        pkgs = import inputs.unstable {inherit system;};
+        ss = name: {
+          type = "app";
+          program = "${pkgs.callPackage ./modules/homemodules/scripts/${name}.nix {inherit pkgs;}}/bin/${name}";
+        };
+      in {
+        apps.${system} = lib.attrsets.genAttrs (map (a: builtins.substring 0 (builtins.stringLength (builtins.baseNameOf a) - 4) (builtins.baseNameOf a)) (lib.filesystem.listFilesRecursive ./modules/homemodules/scripts)) ss;
+        devShells.${system} = {
+          default = import ./shell.nix {inherit pkgs;};
+          scripts = import ./modules/homemodules/shell.nix {inherit pkgs;};
+        };
+        nixosConfigurations = lib.attrsets.genAttrs ["malleum" "magnus"] ns;
+        homeConfigurations = {
+          "joshammer@minimus" = lib.homeManagerConfiguration {
+            modules = [./hosts/minimus];
+            extraSpecialArgs = {inherit inputs system;};
+          };
+        };
+      }
+    );
 }
