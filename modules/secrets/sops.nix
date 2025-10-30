1c18e0df8f01f76d07be08215dbe07109daa8980
Josh Hammer <jph33@outlook.com>
2025-10-30 14:21:54 -0400

    sops

--- modules/secrets/sops.nix
+++ modules/secrets/sops.nix
@@ -3,18 +3,24 @@
     imports = [inputs.sops-nix.nixosModules.sops];
 
     sops = {
+      defaultSopsFile = ./default.yaml;
+      age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+      age.keyFile = "${hostConfig.user.configHome}/sops/age/keys.txt";
+
+      secrets = {};
+    };
+  };
+
+  unify.home = {hostConfig, ...}: {
+    imports = [inputs.sops-nix.homeManagerModules.sops];
+    sops = {
+      defaultSopsFile = ./default.yaml;
       age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
       age.keyFile = "${hostConfig.user.configHome}/sops/age/keys.txt";
 
       secrets = {
-        spotify_client_id = {
-          sopsFile = ./default.yaml;
-          key = "spotify_client_id";
-        };
-        github_token = {
-          sopsFile = ./default.yaml;
-          key = "github_token";
-        };
+        spotify_client_id = {};
+        github_token = {};
       };
     };
   };
