7f622b3694d4f47c07442597e2e1391f864cc1f5
Josh Hammer <jph33@outlook.com>
2026-01-05 14:03:50 -0500

    duod

--- modules/scripts/_duod.nix
+++ modules/scripts/_duod.nix
@@ -32,6 +32,6 @@ pkgs.writeShellScriptBin "duod" ''
   p4=$(echo "$rem/1" | bc)
 
   declare -A digit
-  digit=( [0]=0 [1]=1 [2]=2 [3]=3 [4]=4 [5]=5 [6]=6 [7]=7 [8]=8 [9]=9 [10]=a [11]=b )
+  digit=( [0]=0 [1]=1 [2]=2 [3]=3 [4]=4 [5]=5 [6]=6 [7]=7 [8]=8 [9]=9 [10]=χ [11]=ε )
   echo "''${digit[''$p0]}''${digit[''$p1]}''${digit[''$p2]}''${digit[''$p3]}''${digit[''$p4]}"
 ''
