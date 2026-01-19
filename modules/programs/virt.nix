023e130a40cff7eb2ba144810661d4f204f42415
Joshua Hammer <jph33@outlook.com>
2026-01-19 18:32:54 -0500

    quickemu on mangus

--- modules/programs/virt.nix
+++ modules/programs/virt.nix
@@ -1,17 +1,20 @@
 {
   unify.modules.vrt.nixos = {hostConfig, ...}: {
-    virtualisation.virtualbox.host = {enable = true;};
+    # virtualisation.virtualbox.host = {enable = true;};
 
-    users.users.${hostConfig.user.username}.extraGroups = ["adbuser" "vboxusers"];
+    users.users.${hostConfig.user.username}.extraGroups = [
+      "adbuser"
+      # "vboxusers"
+    ];
   };
 
   unify.modules.vrt.home = {pkgs, ...}: {
-    dconf.settings = {
-      "org/virt-manager/virt-manager/connections" = {
-        autoconnect = ["qemu:///system"];
-        uris = ["qemu:///system"];
-      };
-    };
+    # dconf.settings = {
+    #   "org/virt-manager/virt-manager/connections" = {
+    #     autoconnect = ["qemu:///system"];
+    #     uris = ["qemu:///system"];
+    #   };
+    # };
     home.packages = with pkgs; [
       nixos-shell
       quickemu
