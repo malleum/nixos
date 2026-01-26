dca564636c22693641d77b5bdd5ac1a4527db5f5
Josh Hammer <jph33@outlook.com>
2026-01-26 11:56:56 -0500

    zelli

--- modules/programs/zellij.nix
+++ modules/programs/zellij.nix
@@ -54,6 +54,46 @@
         show_startup_tips = false;
         default_shell = "${pkgs.fish}/bin/fish";
         ui.pane_frames.rounded_corners = true;
+
+        keybinds = {
+          "shared_except \"locked\"" = {
+            # Tab Navigation (Native, no pane creation side-effect)
+            "bind \"Alt 1\"" = { GoToTab = 1; };
+            "bind \"Alt 2\"" = { GoToTab = 2; };
+            "bind \"Alt 3\"" = { GoToTab = 3; };
+            "bind \"Alt 4\"" = { GoToTab = 4; };
+            "bind \"Alt 5\"" = { GoToTab = 5; };
+            "bind \"Alt 6\"" = { GoToTab = 6; };
+            "bind \"Alt 7\"" = { GoToTab = 7; };
+            "bind \"Alt 8\"" = { GoToTab = 8; };
+            "bind \"Alt 9\"" = { GoToTab = 9; };
+            "bind \"Alt 0\"" = { GoToTab = 10; }; # 0 goes to 10
+
+            # Tab Management
+            "bind \"Alt n\"" = { NewTab = {}; }; # Use this to create new tabs
+
+            # Pane Management
+            "bind \"Alt Shift q\"" = { CloseFocus = {}; };
+            "bind \"Alt Enter\"" = { NewPane = {}; };
+            "bind \"Alt t\"" = { NextSwapLayout = {}; };
+            "bind \"Alt f\"" = { ToggleFloatingPanes = {}; };
+
+            # Move Pane (Rearrange within tab)
+            "bind \"Alt Shift h\"" = { MovePane = "Left"; };
+            "bind \"Alt Shift l\"" = { MovePane = "Right"; };
+            "bind \"Alt Shift j\"" = { MovePane = "Down"; };
+            "bind \"Alt Shift k\"" = { MovePane = "Up"; };
+
+            # Move Focus (Navigate between panes)
+            "bind \"Alt h\"" = { MoveFocus = "Left"; };
+            "bind \"Alt l\"" = { MoveFocus = "Right"; };
+            "bind \"Alt j\"" = { MoveFocus = "Down"; };
+            "bind \"Alt k\"" = { MoveFocus = "Up"; };
+
+            "bind \"Alt =\"" = { Resize = "Increase"; };
+            "bind \"Alt -\"" = { Resize = "Decrease"; };
+          };
+        };
       };
     };
   };
