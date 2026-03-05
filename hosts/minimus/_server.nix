22551f85ca6431857eb37ca222bb9a45d447775b
Josh Hammer <jph33@outlook.com>
2026-03-05 09:48:06 -0500

    ssh pub key revert

--- hosts/minimus/_server.nix
+++ hosts/minimus/_server.nix
@@ -1,7 +1,8 @@
-{config, ...}: {
-  # Avoid "Too many open files" during nix build on small VMs
+let
+  sshPubKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+i3+8ZbQPDjp4Te8k7A11JLxMPMCiciUTtHDLBzLlvNK/F+2+UIviqCePxAkH/TAjjoU55T8ycEPDZr3li5id0T5qnAlAFT5AKtheyg76AHq/7rh+SQISRSdKNiLSblabd8iA62odFvU+7/KfFPj9fdnX5495+f3NkH8a5ZsXYhFxtU7rpEgiYEAWT3/GFb31+MxKTtP8zsCrtybCOJY2uNp6U6PGWcVsgOiS2N8Ew0a3Fb7UCRhdipOV34Nl92EaswBbOt2jjjukkjwFuhlw6gaCRxJM5UsynAu0YDISrW8yI8apI2oT9TCzLtO4IiL9iuGn5AI4wYjrrYdZLbNz ssh-key-2026-02-14";
+in {
   systemd.services.nix-daemon.serviceConfig.LimitNOFILE = 65536;
 
-  users.users.root.openssh.authorizedKeys.keyFiles = [config.sops.secrets.oracle_ssh_public.path];
-  users.users.joshammer.openssh.authorizedKeys.keyFiles = [config.sops.secrets.oracle_ssh_public.path];
+  users.users.root.openssh.authorizedKeys.keys = [sshPubKey];
+  users.users.joshammer.openssh.authorizedKeys.keys = [sshPubKey];
 }
