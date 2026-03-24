e199e0e63c458ef5e08285c31b98fac62ab6e279
Joshua Hammer <jph33@outlook.com>
2026-03-23 22:15:16 -0400

    flake

--- modules/programs/game.nix
+++ modules/programs/game.nix
@@ -1,10 +1,16 @@
 {
-  unify.modules.gam.nixos = {
+  unify.modules.gam.nixos = { pkgs, ... }: {
     services.flatpak.enable = true;
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
+      extraPackages = with pkgs; [
+        gtk3
+        glib
+        xorg.libXrandr
+        xorg.libX11
+      ];
     };
   };
 }
