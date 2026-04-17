01462ab2eac41328e84707b25f4cafcd269d9800
Josh Hammer <jph33@outlook.com>
2026-04-17 16:48:08 -0400

    shell also use unstable pkgs

--- modules/meta/nix.nix
+++ modules/meta/nix.nix
@@ -1,4 +1,4 @@
-{
+{inputs, ...}: {
   unify.nixos = {config, ...}: let
     allowed-users = ["@wheel"];
   in {
@@ -9,28 +9,35 @@
       mode = "0440";
     };
 
-    nix.extraOptions = ''
-      !include ${config.sops.templates.nix-access-tokens.path}
-    '';
+    nix = {
+      # This makes 'nix shell nixpkgs#...' use the same nixpkgs as your system
+      registry.nixpkgs.flake = inputs.nixpkgs;
+      # This makes legacy commands like 'nix-shell -p' use the same nixpkgs
+      nixPath = ["nixpkgs=${inputs.nixpkgs}"];
 
-    nix.settings = {
-      inherit allowed-users;
-      trusted-users = allowed-users;
+      extraOptions = ''
+        !include ${config.sops.templates.nix-access-tokens.path}
+      '';
 
-      auto-optimise-store = true;
+      settings = {
+        inherit allowed-users;
+        trusted-users = allowed-users;
 
-      experimental-features = [
-        "flakes"
-        "nix-command"
-      ];
-      substituters = [
-        "https://cache.nixos.org/"
-        "https://hyprland.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
+        auto-optimise-store = true;
+
+        experimental-features = [
+          "flakes"
+          "nix-command"
+        ];
+        substituters = [
+          "https://cache.nixos.org/"
+          "https://hyprland.cachix.org"
+        ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        ];
+      };
     };
   };
 }
