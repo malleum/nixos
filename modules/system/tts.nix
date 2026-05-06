a7fbc5205d1e7150780013862bca89bb5f12ab06
Josh Hammer <jph33@outlook.com>
2026-05-06 11:58:30 -0400

    say command

--- modules/system/tts.nix
+++ modules/system/tts.nix
@@ -1,26 +1,30 @@
 {
-  unify.modules.gui.home = {pkgs, ...}: {
-    xdg.configFile = let
-      piperVoiceOnnx = pkgs.fetchurl {
-        url = "https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/no/no_NO/talesyntese/medium/no_NO-talesyntese-medium.onnx";
-        sha256 = "04qc4wqxbig2b36vyxdr4gjzy1cg8j5lnk07qhl4cb770azawqxp";
-        name = "no_NO-talesyntese-medium.onnx";
-      };
-      piperVoiceJson = pkgs.fetchurl {
-        url = "https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/no/no_NO/talesyntese/medium/no_NO-talesyntese-medium.onnx.json";
-        sha256 = "1m89365awygmdyrhcr1ifj9gav1vfzwarj7a5cyq9ycbzj8h9sxh";
-        name = "no_NO-talesyntese-medium.onnx.json";
-      };
-      voiceDir = pkgs.runCommand "piper-voice-no-talesyntese" {} ''
-        mkdir -p $out
-        cp ${piperVoiceOnnx} $out/no_NO-talesyntese-medium.onnx
-        cp ${piperVoiceJson} $out/no_NO-talesyntese-medium.onnx.json
-      '';
-      sdGeneric = "${pkgs.speechd}/libexec/speech-dispatcher-modules/sd_generic";
-      piperBin = "${pkgs.piper-tts}/bin/piper";
-      aplayBin = "${pkgs.alsa-utils}/bin/aplay";
-      voiceModel = "${voiceDir}/no_NO-talesyntese-medium.onnx";
-    in {
+  unify.modules.gui.home = {pkgs, ...}: let
+    piperVoiceOnnx = pkgs.fetchurl {
+      url = "https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/no/no_NO/talesyntese/medium/no_NO-talesyntese-medium.onnx";
+      sha256 = "04qc4wqxbig2b36vyxdr4gjzy1cg8j5lnk07qhl4cb770azawqxp";
+      name = "no_NO-talesyntese-medium.onnx";
+    };
+    piperVoiceJson = pkgs.fetchurl {
+      url = "https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/no/no_NO/talesyntese/medium/no_NO-talesyntese-medium.onnx.json";
+      sha256 = "1m89365awygmdyrhcr1ifj9gav1vfzwarj7a5cyq9ycbzj8h9sxh";
+      name = "no_NO-talesyntese-medium.onnx.json";
+    };
+    voiceDir = pkgs.runCommand "piper-voice-no-talesyntese" {} ''
+      mkdir -p $out
+      cp ${piperVoiceOnnx} $out/no_NO-talesyntese-medium.onnx
+      cp ${piperVoiceJson} $out/no_NO-talesyntese-medium.onnx.json
+    '';
+    sdGeneric = "${pkgs.speechd}/libexec/speech-dispatcher-modules/sd_generic";
+    piperBin = "${pkgs.piper-tts}/bin/piper";
+    aplayBin = "${pkgs.alsa-utils}/bin/aplay";
+    voiceModel = "${voiceDir}/no_NO-talesyntese-medium.onnx";
+
+    sayCmd = pkgs.writeShellScriptBin "say" ''
+      ${piperBin} -m ${voiceModel} --output-raw | ${aplayBin} -r 22050 -f S16_LE -c 1 -t raw -
+    '';
+  in {
+    xdg.configFile = {
       "speech-dispatcher/speechd.conf".text = ''
         LogLevel 3
         LogDir "default"
@@ -44,6 +48,7 @@
       speechd
       piper-tts
       alsa-utils
+      sayCmd
     ];
   };
 }
